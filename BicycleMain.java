f82fc2e8bb45731cdde3593d045003f2da7daf8f
darlo2003 <baymax162003@gmail.com>
2024-10-04 15:04:33 +0800

    Delete BicycleMain.java

--- BicycleMain.java
+++
@@ -1,9 +0,0 @@
-public class BicycleMain
-{
-	public static void main(String[] args) {
-		Bicycle sportsBicycle = new Bicycle();
-		int numGears = sportsBicycle.gear;
-		System.out.println("Number of Gears: " + numGears);
-		sportsBicycle.braking();
-	}
-}
