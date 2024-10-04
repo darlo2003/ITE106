94c0c97b481f381819974e5e7d3304562c259438
darlo2003 <baymax162003@gmail.com>
2024-10-04 14:17:01 +0800

    03TextFileManipulationScannerWithoutLoop.java

---
+++ 03TextFileManipulationScannerWithoutLoop.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+ 
+public class ReadingEntireFileWithoutLoop 
+{
+    public static void main(String[] args)
+        throws FileNotFoundException
+    {
+        File file = new File("C:\\Users\\pankaj\\Desktop\\test.txt");
+        Scanner sc = new Scanner(file);
+ 
+        // we just need to use \\Z as delimiter
+        sc.useDelimiter("\\Z");
+ 
+        System.out.println(sc.next());
+    }
+}
