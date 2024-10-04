9d4e6cd6c369a89c016a6acec419c00d06607be0
darlo2003 <baymax162003@gmail.com>
2024-10-04 14:33:11 +0800

    Update 02TextFileManipulationScannerLoop.java

--- 02TextFileManipulationScannerLoop.java
+++ 02TextFileManipulationScannerLoop.java
@@ -1,14 +1,34 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
-public class ReadFromFileUsingScanner 
-{
-    public static void main(String[] args) throws Exception
-    {
-        // pass the path to the file as a parameter
-        File file = new File("");
-        Scanner sc = new Scanner(file);
- 
-        while (sc.hasNextLine())
-            System.out.println(sc.nextLine());
+
+public class TextFileManipulationScannerLoop {
+    public static void main(String[] args) {
+        String inputFilePath = "input.txt"; 
+        String outputFilePath = "output.txt"; 
+
+        try {
+            
+            Scanner scanner = new Scanner(new File(inputFilePath));
+            FileWriter writer = new FileWriter(outputFilePath);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                
+                String manipulatedLine = line.toUpperCase();
+                
+                writer.write(manipulatedLine + System.lineSeparator());
+            }
+
+            scanner.close();
+            writer.close();
+            System.out.println("File manipulation complete. Check output.txt.");
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error writing to output file: " + e.getMessage());
+        }
     }
 }
