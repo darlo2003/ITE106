5844cdb88d54d64247f7bba461e8053b6a975ef0
darlo2003 <baymax162003@gmail.com>
2024-10-04 14:24:38 +0800

    Create 05TextFileManipulationFileReader.java

---
+++ 05TextFileManipulationFileReader.java
@@ -0,0 +1,16 @@
+import java.io.*;
+ 
+public class GFG {
+ 
+    public static void main(String[] args) throws Exception
+    {
+ 
+        FileReader fr = new FileReader("C:\\Users\\pankaj\\Desktop\\test.txt");
+ 
+        int i;
+      
+        while ((i = fr.read()) != -1)
+ 
+            System.out.print((char)i);
+    }
+}
