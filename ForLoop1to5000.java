4517bc25a8b72951d3d6ccfdf32ce06b1233006a
darlo2003 <baymax162003@gmail.com>
2024-10-04 15:32:07 +0800

    Update ForLoop1to5000.java

--- ForLoop1to5000.java
+++ ForLoop1to5000.java
@@ -4,7 +4,7 @@ public class ForLoop1to5000
 {
 	public static void main(String[]args) {
 	for (int i = 1; i <= 5000; ++i) {
-		System.out.println("Darlo Medina V.");
+		System.out.println("Medina Darlo V.");
 		}
 	}
 }
